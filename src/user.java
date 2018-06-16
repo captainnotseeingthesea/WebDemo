3e8674c77bbf3b64ef8b21294610319eeb8ff10a
LiXuanYi <57436746@qq.com>
2018-06-16 22:03:05 +0800

    初次练习git

--- src/user.java
+++ src/user.java
@@ -18,5 +18,31 @@
  * @since 1.0.0
  */
 public class user {
+    private String username;//用户名
+    private String password;//用户密码
+    private int id;//用户id号
 
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
 }
