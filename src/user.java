51c9340f75a0c22c00219eae2d8d9e037d67b87a
LiXuanYi <57436746@qq.com>
2018-06-16 21:50:32 +0800

    初次练习git

---
+++ src/user.java
@@ -0,0 +1,22 @@
+/**
+ * Copyright (C), 2015-2018, 华电408有限公司
+ * FileName: user
+ * Author:   宣佚
+ * Date:     2018/6/16 0016 下午 21:38
+ * Description: 用户JavaBean
+ * History:
+ * <author>          <time>          <version>          <desc>
+ * 作者姓名           修改时间           版本号              描述
+ */
+
+/**
+ * 〈一句话功能简述〉<br> 
+ * 〈用户JavaBean〉
+ *
+ * @author 宣佚
+ * @create 2018/6/16 0016
+ * @since 1.0.0
+ */
+public class user {
+
+}
---
+++ web/index.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: 宣佚
+  Date: 2018/6/16 0016
+  Time: 下午 18:30
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+  <head>
+    <title>WebDemo</title>
+  </head>
+  <body>
+  李宣佚
+  </body>
+</html>
